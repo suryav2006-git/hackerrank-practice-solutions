cdcbbf4076a611540be5333276480a89df81b990
Surya V <mrstudieshelper@gmail.com>
2026-06-03 10:02:31 +0530

    Added solution - LeetHub

---
+++ 8038-say-hello-world-with-c-Hackerrank/8038-say-hello-world-with-c.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */      
+    cout << "Hello, World!" ;
+    return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
