25df221dd744d9c32c69e401c4ff84a1c497bd17
Surya V <mrstudieshelper@gmail.com>
2026-06-03 09:55:55 +0530

    Added solution - LeetHub

---
+++ 12103-input-and-output-Hackerrank/12103-input-and-output.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */     
+    int a, b, c;
+     
+    cin >> a >> b >> c ;
+    int sum = a + b + c;   
+    cout << sum ;
+    return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
